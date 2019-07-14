327e8d1a6752386887cf573f7e0eca13448f8da5
jonathanharmatz <jonathanharmatz@gmail.com>
2019-07-14 15:19:08 +0300

    State Exrc

---
+++ State/IAmBroken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class IAmBroken : ILightState
+    {
+        public ILightState Fix()
+        {
+            Console.WriteLine("Switch fixed, light is now off");
+
+            return new IAmOff();
+        }
+
+        public ILightState TurnOff()
+        {
+            Console.WriteLine("Switch is broken! Can't turn off the light");
+            return this;
+        }
+
+        public ILightState TurnOn()
+        {
+            Console.WriteLine("Switch is broken! Can't turn on the light");
+            return this;
+        }
+        
+
+    }
+}
---
+++ State/IAmOn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class IAmOn : ILightState
+    {
+        public ILightState TurnOff()
+        {
+            Console.WriteLine("Turning off the light....");
+            return new IAmOff();
+        }
+
+        public ILightState TurnOn()
+        {
+            Console.WriteLine("You broke the switch!");
+            return new IAmBroken();
+        }
+        public ILightState Fix()
+        {
+            Console.WriteLine("Switch is not broken, nothing to fix");
+
+            return this;
+        }
+    }
+}
+
