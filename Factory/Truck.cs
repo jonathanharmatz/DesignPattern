d5b392c8c225ae6d54007340aa66b05e7e7f2124
jonathanharmatz <jonathanharmatz@gmail.com>
2019-07-14 14:05:59 +0300

    Singelton & Factory Exercises

---
+++ Factory/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExrc
+{
+
+    public class Truck : Vehicle
+    {
+
+        protected string hunk;
+
+
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string hunk) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.hunk = hunk;
+        }
+
+        public override string ToString()
+        {
+            return "Truck : " + base.ToString() + $" { hunk}";
+        }
+    }
+}
---
+++ Factory/Unicycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExrc
+{
+    public class Unicycle : Vehicle
+    {
+
+        protected string ring;
+
+        public Unicycle(string model, float cylinder, string color, int numberOfWheels, string ring) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.ring = ring;
+        }
+
+        public override string ToString()
+        {
+            return "Unicycle : " + base.ToString() + $" { ring }";
+        }
+    }
+}
+
