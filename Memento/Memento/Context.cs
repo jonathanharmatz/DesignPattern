7b45ba9931087522dd0c9b3c25277dee49c161e2
jonathanharmatz <jonathanharmatz@gmail.com>
2019-07-16 14:49:11 +0300

    Memento Exrc

---
+++ Memento/Memento/Context.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memento
+{
+    public class Context
+    {
+
+        TextCareTaker careTake = new TextCareTaker();
+
+        public void Save(RichTextBox richTex)
+        {
+            careTake.AddMemento(new TextMemento(richTex.Text));
+        }
+
+        public void Load(RichTextBox richText)
+        {
+            TextMemento lastMemento = careTake.GetPreviouse();
+
+            if (lastMemento != null && lastMemento.savedText != null)
+                richText.Text = lastMemento.savedText;
+        }
+
+        public void Next(RichTextBox richText)
+        {
+            TextMemento nextMemento = careTake.GetNext();
+
+            if (nextMemento != null && nextMemento.savedText != null)
+                richText.Text = nextMemento.savedText;
+        }
+    }
+}
---
+++ Memento/Memento/TextCareTaker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class TextCareTaker
+    {
+        List<TextMemento> mementos= new List<TextMemento>();
+        int currMemento = 0;
+
+        public void AddMemento(TextMemento memento)
+        {
+
+            this.mementos.Add(memento);
+
+            if (this.currMemento != this.mementos.Count - 1)
+            {
+                this.mementos.RemoveRange(this.currMemento, this.mementos.Count - this.currMemento);
+                this.currMemento = this.mementos.Count - 1;
+            }
+            else
+            {
+                this.currMemento++;
+
+            }
+        }
+
+        public TextMemento GetPreviouse()
+        {
+            if (this.currMemento > 0)
+                this.currMemento--;
+            else
+                return null;
+
+            return this.mementos[this.currMemento];
+        }
+
+        public TextMemento GetNext()
+        {
+            if (this.currMemento < this.mementos.Count - 1)
+                this.currMemento++;
+            else
+                return null;
+
+            return this.mementos[this.currMemento];
+        }
+    }
+}
